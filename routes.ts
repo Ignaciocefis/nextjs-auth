8fa624be4670c7e9a547f7ecd0f9f5b25cc974e4
Ignaciocefis <ignaciocefis2001@gmail.com>
2024-08-01 12:14:07 +0200

    Middleware and Login

---
+++ routes.ts
@@ -0,0 +1,12 @@
+export const publicRoutes = [
+  '/'
+]
+
+export const authRoute = [
+  '/login',
+  '/register'
+]
+
+export const apiAuthPrefix = '/api/auth'
+
+export const DEFAULT_LOGIN_REDIRECT = '/settings'
